772ad582cdc7289b9b663ea9c59bc7c921f5855d
Petr Belohlavek <me@petrbel.cz>
2017-03-06 16:28:39 +0100

    checking dataset interface

--- cxflow/dataset_loader.py
+++ cxflow/dataset_loader.py
@@ -29,18 +29,39 @@ class DatasetLoader:
 
         self.load_f = getattr(self, '_load_{}'.format(self.config['dataset']['backend']))
 
+    @staticmethod
+    def _verify_dataset(dataset):
+        """Verify the passed dataset implements the interface of AbstractDataset."""
+
+        for method_name in dir(AbstractDataset):
+            if callable(getattr(AbstractDataset, method_name)):
+                try:
+                    method = getattr(dataset, method_name)
+                    if not callable(method):
+                        raise ValueError()
+                except:
+                    logging.error('Dataset does not contain callable method "%s"', method_name)
+                    raise ValueError()
+
     def load_dataset(self) -> AbstractDataset:
         """Load the dataset by using the proper backend."""
-        return self.load_f()
+
+        logging.debug('Loading the dataset')
+        dataset =  self.load_f()
+
+        logging.debug('Checking the dataset')
+        DatasetLoader._verify_dataset(dataset)
+
+        return dataset
 
     def _load_fuel(self) -> AbstractDataset:
         """Load the dataset by using Fuel (Python) backend"""
+
         logging.debug('Using fuel backend')
         return self.dataset_class(**self.config['dataset'], **self.config['stream'])
 
     def _load_cxtream(self) -> AbstractDataset:
         """Load the dataset by using cxtream (C++) backend"""
+
         logging.debug('Using cxtream backend')
-        dataset = self.dataset_class(self.dumped_cofig_path)
-        # TODO: assert
-        return dataset
+        return self.dataset_class(self.dumped_cofig_path)
