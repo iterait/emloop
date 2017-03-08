dd1219bbddff224d0775e431d77da0996e9bf47a
Petr Belohlavek <me@petrbel.cz>
2017-03-08 12:36:55 +0100

    subcommands + restructure

--- cxflow/entry_point.py
+++ cxflow/entry_point.py
@@ -3,6 +3,7 @@
 from .network_manager import NetworkManager
 from .utils.arg_parser import parse_arg
 from .dataset_loader import DatasetLoader
+from .hooks.abstract_hook import AbstractHook
 
 from argparse import ArgumentParser
 from datetime import datetime
@@ -18,62 +19,6 @@ import ruamel.yaml
 
 class EntryPoint:
     """Entry point of the whole training. Should be used only via `cxflow` command."""
-
-    def __init__(self):
-        """
-        Entry point constructor.
-
-        All arguments are passed via CLI arguments which should be in form of `key[:type]=value`.
-        Then the life cycle is as follows:
-        1. configuration file is loaded
-        2. CLI arguments are applied
-        3. training directory is created
-        4. final configuration is dumped
-        5. dataset is loaded
-        6. network is created
-        """
-
-        parser = ArgumentParser('cxflow')
-        parser.add_argument('config_file', help='path to the config file')
-        parser.add_argument('-o',  '--output-root', default='log', help='output directory')
-        parser.add_argument('-v',  '--verbose', action='store_true', help='incease verbosity do level DEBUG')
-        known_args, unknown_args = parser.parse_known_args()
-
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                            level=logging.DEBUG if known_args.verbose else logging.INFO)
-
-        self.net = None
-        self._output_root = known_args.output_root
-
-        try:
-            self._load_config(config_file=known_args.config_file, additional_args=unknown_args)
-        except Exception as e:
-            logging.error('Loading config failed: %s', e)
-            quit()
-
-        try:
-            self.output_dir = self._create_output_dir()
-        except Exception as e:
-            logging.error('Creating output dir failed: %s', e)
-            quit()
-
-        try:
-            self.dumped_config_file = self._dump_config()
-        except Exception as e:
-            logging.error('Saving modified config failed: %s', e)
-            quit()
-
-        try:
-            self._create_dataset()
-        except Exception as e:
-            logging.error('Creating dataset failed: %s', e)
-            quit()
-
-        try:
-            self._create_network()
-        except Exception as e:
-            logging.error('Creating network failed: %s', e)
-            quit()
 
     def _load_config(self, config_file: str, additional_args: typing.Iterable[str]) -> dict:
         """Load config from `config_file` and apply CLI args `additional_args`. The result is saved as `self.config`"""
@@ -139,43 +84,152 @@ class EntryPoint:
             yaml.dump(self.config, f)
         return dumped_config_f
 
-    def run(self) -> None:
-        """Construct hooks, create `NetworkManager` and run the main loop."""
+    def _construct_hook(self, hook_module: str, hook_class, **kwargs) -> AbstractHook:
+        """
+        Construct a hook.
+
+        This is equivalent to
+        ```
+        from <hook_module> import <hook_class>
+        return <hook_class>(**kwargs)
+        ```
+        """
+
+        logging.debug('Loading hook module %s', hook_module)
+        hook_module = importlib.import_module(hook_module)
+
+        logging.debug('Loading hook class %s', hook_class)
+        hook_class = getattr(hook_module, hook_class)
+
+        logging.debug('Constructing hook')
+        hook = hook_class(net=self.net, config=self.config, **kwargs)
+        return hook
+
+    def _construct_hooks(self) -> typing.Iterable[AbstractHook]:
+        """Construct hooks from the saved config. file."""
 
         hooks = []
         if 'hooks' in self.config:
             logging.info('Creating hooks')
             for hook_conf in self.config['hooks']:
-                if 'hook_module' not in hook_conf or 'hook_class' not in hook_conf:
-                    logging.error('All hook configs must contain hook_module and hook_class')
-                    quit()
-
-                logging.debug('Loading hook module %s', hook_conf['hook_module'])
-                hook_module = importlib.import_module(hook_conf['hook_module'])
-
-                logging.debug('Loading hook class %s', hook_conf['hook_class'])
-                hook_class = getattr(hook_module, hook_conf['hook_class'])
-
-                logging.debug('Constructing hook')
-                hook = hook_class(net=self.net, config=self.config, **hook_conf)
-
-                hooks.append(hook)
+                try:
+                    hook = self._construct_hook(**hook_conf)
+                    hooks.append(hook)
+                except Exception as e:
+                    logging.error('Unexpected exception occurred when constructing hook with config: "%s".'
+                                  'Exception: %"', hook_conf, e)
+                    raise e
+            return hooks
         else:
             logging.warning('No hooks found')
+            return []
 
-        logging.info('Creating NetworkManager')
-        manager = NetworkManager(net=self.net,
-                                 dataset=self.dataset,
-                                 hooks=hooks)
-        logging.info('Running main loop')
-        manager.run_main_loop(batch_size=self.config['net']['batch_size'])
+    def train(self, config_file: str, output_root: str, cli_options: typing.Iterable[str]) -> None:
+        """
+        Train method resposible for constring all required objects and training itself.
+
+        All arguments are passed via CLI arguments which should be in form of `key[:type]=value`.
+        Then the life cycle is as follows:
+        1. configuration file is loaded
+        2. CLI arguments are applied
+        3. training directory is created
+        4. final configuration is dumped
+        5. dataset is loaded
+        6. network is created
+        7. mainloop hooks are created
+        8. NetworkManager is created
+        9. mainloop is initiated
+        """
+
+        self.net = None
+        self._output_root = output_root
+
+        try:
+            self._load_config(config_file=config_file, additional_args=cli_options)
+        except Exception as e:
+            logging.error('Loading config failed: %s', e)
+            sys.exit(1)
+
+        try:
+            self.output_dir = self._create_output_dir()
+        except Exception as e:
+            logging.error('Creating output dir failed: %s', e)
+            sys.exit(1)
+
+        try:
+            self.dumped_config_file = self._dump_config()
+        except Exception as e:
+            logging.error('Saving modified config failed: %s', e)
+            sys.exit(1)
+
+        try:
+            self._create_dataset()
+        except Exception as e:
+            logging.error('Creating dataset failed: %s', e)
+            sys.exit(1)
+
+        try:
+            self._create_network()
+        except Exception as e:
+            logging.error('Creating network failed: %s', e)
+            sys.exit(1)
+
+        try:
+            hooks = self._construct_hooks()
+        except Exception as e:
+            logging.error('Creating hooks failed: %s', e)
+            sys.exit(1)
+
+        try:
+            logging.info('Creating NetworkManager')
+            manager = NetworkManager(net=self.net,
+                                     dataset=self.dataset,
+                                     hooks=hooks)
+        except Exception as e:
+            logging.error('Creating NetworkManager failed: %s', e)
+            sys.exit(1)
+
+        try:
+            logging.info('Running main loop')
+            manager.run_main_loop(batch_size=self.config['net']['batch_size'])
+        except Exception as e:
+            logging.error('Running the main loo failed: %s', e)
+            sys.exit(1)
 
 
 def init_entry_point() -> None:
     sys.path.insert(0, os.getcwd())
 
+    parser = ArgumentParser('cxflow')
+    subparsers = parser.add_subparsers(help='cxflow modes')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase verbosity do level DEBUG')
+
+    train_parser = subparsers.add_parser('train')
+    train_parser.set_defaults(subcommand='train')
+    train_parser.add_argument('config_file', help='path to the config file')
+    train_parser.add_argument('-o', '--output-root', default='log', help='output directory')
+
+    crossval_split_parser = subparsers.add_parser('xval-init')
+    crossval_split_parser.set_defaults(subcommand='xval-init')
+    crossval_split_parser.add_argument('-s', '--seed', type=int, default=100003, help='split seed')
+
+    known_args, unknown_args = parser.parse_known_args()
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                        level=logging.DEBUG if known_args.verbose else logging.INFO)
+
     entry_point = EntryPoint()
-    entry_point.run()
+    if known_args.subcommand == 'train':
+        entry_point.train(config_file=known_args.config_file,
+                          output_root=known_args.output_root,
+                          cli_options=unknown_args)
+
+    elif known_args.subcommand == 'xval-init':
+        raise NotImplementedError()
+
+    else:
+        logging.error('Unrecognized subcommand: "%s". Please run `cxflow -h` for more info.', known_args.subcommand)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
