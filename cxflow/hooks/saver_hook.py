f47d7c7e76f5d6f75e274f81f86565ff2d107e88
Petr Belohlavek <me@petrbel.cz>
2017-03-15 17:10:13 +0100

    saving_hooks private attrs

--- cxflow/hooks/saver_hook.py
+++ cxflow/hooks/saver_hook.py
@@ -13,14 +13,14 @@ class SaverHook(AbstractHook):
         :param save_every_n_epochs: how often is the model saved
         """
         super().__init__(net=net, **kwargs)
-        self.net = net
-        self.save_every_n_epochs = save_every_n_epochs
+        self._net = net
+        self._save_every_n_epochs = save_every_n_epochs
 
     def after_epoch(self, epoch_id: int, **kwargs) -> None:
-        if epoch_id % self.save_every_n_epochs == 0:
+        if epoch_id % self._save_every_n_epochs == 0:
             try:
                 logging.debug('Creating checkpoint')
-                save_path = self.net.save_checkpoint(name=str(epoch_id))
+                save_path = self._net.save_checkpoint(name=str(epoch_id))
                 logging.info('Created checkpoint: %s', save_path)
             except Exception as e:
                 logging.error('Checkpoint at epoch=%d not created because an unexpected exception occurred: %s',
@@ -39,33 +39,33 @@ class BestSaverHook(AbstractHook):
         :param output_name: suffix of the dumped checkpoint
         """
         super().__init__(net=net, **kwargs)
-        self.net = net
-        self.metric = metric
-        self.condition = condition
-        self.output_name = output_name
+        self._net = net
+        self._metric = metric
+        self._condition = condition
+        self._output_name = output_name
 
         self.best_metric = None
 
     def _save_checkpoint(self):
         try:
             logging.debug('Creating checkpoint')
-            save_path = self.net.save_checkpoint(name=self.output_name)
+            save_path = self._net.save_checkpoint(name=self._output_name)
             logging.info('Created checkpoint: %s', save_path)
         except Exception as e:
             logging.error('Checkpoint not created because an unexpected exception occurred: %s', e)
 
     def before_first_epoch(self, valid_results: dict, **kwargs) -> None:
-        self.best_metric = valid_results[self.metric]
+        self.best_metric = valid_results[self._metric]
         self._save_checkpoint()
 
     def after_epoch(self, valid_results: dict, **kwargs) -> None:
-        if self.condition == 'min':
-            if self.best_metric is None or valid_results[self.metric] < self.best_metric:
-                self.best_metric = valid_results[self.metric]
+        if self._condition == 'min':
+            if self.best_metric is None or valid_results[self._metric] < self.best_metric:
+                self.best_metric = valid_results[self._metric]
                 self._save_checkpoint()
-        elif self.condition == 'max':
-            if self.best_metric is None or valid_results[self.metric] > self.best_metric:
-                self.best_metric = valid_results[self.metric]
+        elif self._condition == 'max':
+            if self.best_metric is None or valid_results[self._metric] > self.best_metric:
+                self.best_metric = valid_results[self._metric]
                 self._save_checkpoint()
         else:
             logging.error('BestSaverHook support only {min,max} as a condition')
