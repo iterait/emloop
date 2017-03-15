82ef8beed396571a4350faeb389a8cf46f1183be
Petr Belohlavek <me@petrbel.cz>
2017-03-15 17:09:02 +0100

    result_hook private attrs

--- cxflow/hooks/result_hook.py
+++ cxflow/hooks/result_hook.py
@@ -20,34 +20,34 @@ class ResultHook(AbstractHook):
         :param output_prefix: prefix of the dumped file
         """
         super().__init__(net=net, **kwargs)
-        self.net = net
-        self.metric = metric
-        self.condition = condition
-        self.metrics_to_log = metrics_to_log
+        self._net = net
+        self._metric = metric
+        self._condition = condition
+        self._metrics_to_log = metrics_to_log
 
-        self.valid_f = path.join(self.net.log_dir, output_prefix+'_valid.csv')
-        self.test_f = path.join(self.net.log_dir, output_prefix+'_test.csv')
+        self._valid_f = path.join(self._net.log_dir, output_prefix + '_valid.csv')
+        self._test_f = path.join(self._net.log_dir, output_prefix + '_test.csv')
 
-        self.best_metric = None
+        self._best_metric = None
 
-        logging.info('Results will be saved to "%s" and "%s"', self.valid_f, self.test_f)
+        logging.info('Results will be saved to "%s" and "%s"', self._valid_f, self._test_f)
         self._reset()
 
     def _reset(self):
         """Reset all buffers."""
 
-        self.valid_buffer = []
-        self.test_buffer = []
+        self._valid_buffer = []
+        self._test_buffer = []
 
     def _save_partial_results(self, buffer: list, results: dict):
         """Append result to a buffer"""
 
-        for i in range(len(results[self.metrics_to_log[0]])):
-            buffer.append([results[metric][i] for metric in self.metrics_to_log])
+        for i in range(len(results[self._metrics_to_log[0]])):
+            buffer.append([results[metric][i] for metric in self._metrics_to_log])
 
     def _save_results(self, buffer: list, f_name: str):
         with open(f_name, 'w') as f:
-            header = ','.join(['"{}"'.format(metric) for metric in self.metrics_to_log]) + '\n'
+            header = ','.join(['"{}"'.format(metric) for metric in self._metrics_to_log]) + '\n'
             f.write(header)
 
             for result_row in buffer:
@@ -60,31 +60,31 @@ class ResultHook(AbstractHook):
         if stream_type == 'train':
             pass
         elif stream_type == 'valid':
-            self._save_partial_results(self.valid_buffer, results)
+            self._save_partial_results(self._valid_buffer, results)
         elif stream_type == 'test':
-            self._save_partial_results(self.test_buffer, results)
+            self._save_partial_results(self._test_buffer, results)
         else:
             raise ValueError('stream_type must be either train, valid or test. Instead, `%s` was'
                              'provided'.format(stream_type))
 
     def before_first_epoch(self, valid_results: dict, **kwargs) -> None:
-        self.best_metric = valid_results[self.metric]
-        self._save_results(self.valid_buffer, self.valid_f)
-        self._save_results(self.test_buffer, self.test_f)
+        self._best_metric = valid_results[self._metric]
+        self._save_results(self._valid_buffer, self._valid_f)
+        self._save_results(self._test_buffer, self._test_f)
 
     def after_epoch(self, valid_results: dict, **kwargs) -> None:
-        if self.condition == 'min':
-            if self.best_metric is None or valid_results[self.metric] < self.best_metric:
+        if self._condition == 'min':
+            if self._best_metric is None or valid_results[self._metric] < self._best_metric:
                 logging.info('Saving results')
-                self.best_metric = valid_results[self.metric]
-                self._save_results(self.valid_buffer, self.valid_f)
-                self._save_results(self.test_buffer, self.test_f)
-        elif self.condition == 'max':
-            if self.best_metric is None or valid_results[self.metric] > self.best_metric:
+                self._best_metric = valid_results[self._metric]
+                self._save_results(self._valid_buffer, self._valid_f)
+                self._save_results(self._test_buffer, self._test_f)
+        elif self._condition == 'max':
+            if self._best_metric is None or valid_results[self._metric] > self._best_metric:
                 logging.info('Saving results')
-                self.best_metric = valid_results[self.metric]
-                self._save_results(self.valid_buffer, self.valid_f)
-                self._save_results(self.test_buffer, self.test_f)
+                self._best_metric = valid_results[self._metric]
+                self._save_results(self._valid_buffer, self._valid_f)
+                self._save_results(self._test_buffer, self._test_f)
         else:
             logging.error('BestSaverHook support only {min,max} as a condition')
             raise ValueError('BestSaverHook support only {min,max} as a condition')
