d8d8697437d2d341760c0341dff43a11e0745ef7
blazeka <blazekada@gmail.com>
2017-03-24 09:38:43 +0100

    Add Timer tests

---
+++ cxflow/tests/utils/profiling_test.py
@@ -0,0 +1,63 @@
+from cxflow.utils.profile import Timer
+
+import logging
+import time
+from unittest import TestCase
+
+
+class ParseArgumentsTest(TestCase):
+    def __init__(self, *args, **kwargs):
+        logging.getLogger().disabled = True
+        super().__init__(*args, **kwargs)
+
+    def test_empty_timer(self):
+        log = {}
+        with Timer('noop', log):
+            pass
+
+        self.assertIn('noop', log)
+        self.assertEqual(len(log['noop']), 1)
+        self.assertAlmostEqual(log['noop'][0], 0, places=5)
+
+    def test_appending(self):
+        log = {}
+
+        n_iter = 100
+
+        for i in range(n_iter):
+            with Timer('noop', log):
+                pass
+
+        self.assertEqual(len(log['noop']), n_iter)
+
+    def test_nested(self):
+        log = {}
+
+        with Timer('timerop', log):
+            with Timer('noop', log):
+                pass
+
+        self.assertIn('noop', log)
+        self.assertIn('timerop', log)
+
+        self.assertGreater(log['timerop'][0], log['noop'][0])
+
+    def test_time(self):
+        log = {}
+
+        sleep_time = 1
+
+        with Timer('sleep', log):
+            time.sleep(sleep_time)
+
+        self.assertAlmostEqual(log['sleep'][0], sleep_time, places=2)
+
+    def test_order(self):
+        log = {}
+
+        for i in range(3):
+
+            with Timer('sleep', log):
+                time.sleep(i)
+
+            self.assertAlmostEqual(log['sleep'][i], i, places=2)
