7b534fffe0365cacfdfc03bf18f28451eb918b19
blazeka <blazekada@gmail.com>
2017-03-27 11:59:08 +0200

    Refactor AbstractFuelDataset

--- cxflow/datasets/abstract_fuel_dataset.py
+++ cxflow/datasets/abstract_fuel_dataset.py
@@ -1,5 +1,7 @@
 from .abstract_dataset import AbstractDataset
 
+import yaml
+
 from abc import abstractmethod, ABCMeta
 
 
@@ -10,44 +12,72 @@ class AbstractFuelDataset(metaclass=ABCMeta):
     underscored versions should be used.
     """
 
-    def __init__(self, train: dict, valid: dict, **kwargs):
+    def __init__(self, config_str: str):
         """Save all kwargs"""
 
-        self.train = train
-        self.valid = valid
+        config = yaml.load(config_str)
 
-        for name, value in kwargs.items():
-            setattr(self, name, value)
+        assert('dataset' in config)
+        assert('stream' in config)
+
+        output_dir = config['output_dir'] if 'output_dir' in config else None
+
+        self._init_with_kwargs(output_dir=output_dir, **config['dataset'], **config['stream'])
+
+        assert('train' in config['stream'])
+        assert('valid' in config['stream'])
+
+        self._train_config = config['stream']['train']
+        self._valid_config = config['stream']['valid']
+
+        if 'test' in config['stream']:
+            self._test_config = config['stream']['test']
 
     def create_train_stream(self) -> AbstractDataset.Stream:
-        return self._create_train_stream(**self.train)
+        if not hasattr(self, '_train_config'):
+            raise ValueError('Fuel dataset does not have train config initialized.')
+        return self._create_train_stream(**self._train_config)
 
     def create_valid_stream(self) -> AbstractDataset.Stream:
-        return self._create_valid_stream(**self.valid)
+        if not hasattr(self, '_valid_config'):
+            raise ValueError('Fuel dataset does not have valid config initialized.')
+        return self._create_valid_stream(**self._valid_config)
 
+    def create_test_stream(self) -> AbstractDataset.Stream:
+        if not hasattr(self, '_test_config'):
+            raise ValueError('Fuel dataset does not have test config initialized.')
+        return self._create_test_stream(**self._test_config)
+
+    @abstractmethod
+    def _init_with_kwargs(self, **kwargs) -> None:
+        """
+        Dataset initialization with kwargs parsed from the yaml config string.
+        :param kwargs: kwargs parsed from the yaml config
+        """
+        pass
 
     @abstractmethod
     def _create_train_stream(self, **kwargs) -> AbstractDataset.Stream:
-        """Return a train iterator which is parametrized by kwargs"""
+        """
+        Return a train stream iterator parametrized with kwargs.
+        :param kwargs: train stream kwargs
+        :return: train stream iterator
+        """
         pass
 
     @abstractmethod
     def _create_valid_stream(self, **kwargs) -> AbstractDataset.Stream:
-        """Return a valid iterator which is parametrized by kwargs"""
+        """
+        Return a valid stream iterator parametrized with kwargs.
+        :param kwargs: valid stream kwargs
+        :return: valid stream iterator
+        """
         pass
 
-
-class AbstractFuelDatasetWithTest(AbstractFuelDataset, metaclass=ABCMeta):
-    """Same as `AbstractFuelDataset` with test stream methods."""
-
-    def __init__(self, test: dict, **kwargs):
-        super().__init__(**kwargs)
-        self.test = test
-
-    def create_test_stream(self) -> AbstractDataset.Stream:
-        return self._create_test_stream(**self.test)
-
-    @abstractmethod
     def _create_test_stream(self, **kwargs) -> AbstractDataset.Stream:
-        """Return a test iterator which is parametrized by kwargs"""
-        pass
+        """
+        Return a test stream iterator parametrized with kwargs.
+        :param kwargs: test stream kwargs
+        :return: test stream iterator
+        """
+        raise NotImplementedError('Dataset does not implement create test stream although it is required.')
