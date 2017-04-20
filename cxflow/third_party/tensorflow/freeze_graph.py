e6bfa45dcb6637fae2d38729080e6d47bead7a87
Adam Blazek <blazekada@gmail.com>
2017-04-20 13:43:31 +0000

    Code style improvements for save-fix

--- cxflow/third_party/tensorflow/freeze_graph.py
+++ cxflow/third_party/tensorflow/freeze_graph.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# Modifications copyright 2017 Cognexa Solutions s.r.o.
+# ==============================================================================
 """Converts checkpoint variables into Const ops in a standalone GraphDef file.
 
-This script is designed to take a GraphDef proto, a SaverDef proto, and a set of
+This script is designed to take a GraphDef proto, and a set of
 variable values stored in a checkpoint file, and output a GraphDef with all of
 the variable ops converted into const ops containing the values of the
 variables.
@@ -26,104 +28,76 @@ RestoreTensor ops and file loading calls that they rely on.
 You can look at freeze_graph_test.py for an example of how to use it.
 
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from typing import Iterable
 
-import argparse
-import logging
-import sys
-
-from google.protobuf import text_format
-
-from tensorflow.core.framework import graph_pb2
-from tensorflow.core.protobuf import saver_pb2
-from tensorflow.python import pywrap_tensorflow
-from tensorflow.python.client import session
-from tensorflow.python.framework import graph_util
-from tensorflow.python.framework import importer
-from tensorflow.python.platform import gfile
-from tensorflow.python.training import saver as saver_lib
+from ...utils.misc import DisabledLogger, DisabledPrint
+from tensorflow.core.framework import graph_pb2  # pylint: disable=no-name-in-module
+from tensorflow.python import pywrap_tensorflow  # pylint: disable=no-name-in-module
+from tensorflow.python.client import session  # pylint: disable=no-name-in-module
+from tensorflow.python.framework import graph_util  # pylint: disable=no-name-in-module
+from tensorflow.python.framework import importer  # pylint: disable=no-name-in-module
+from tensorflow.python.platform import gfile  # pylint: disable=no-name-in-module
+from tensorflow.python.training import saver as saver_lib  # pylint: disable=no-name-in-module
 
 
-def freeze_graph(input_graph,
-                 input_saver,
-                 input_binary,
-                 input_checkpoint,
-                 output_node_names,
-                 restore_op_name,
-                 filename_tensor_name,
-                 output_graph,
-                 clear_devices,
-                 initializer_nodes,
-                 variable_names_blacklist=""):
-  """Converts all variables in a graph and checkpoint into constants."""
+def freeze_graph(input_graph: str, input_checkpoint: str, output_node_names: Iterable[str], output_graph: str):
+    """
+    Convert all variables in a graph and checkpoint into constants and save the new graph to the specified file.
 
-  del restore_op_name, filename_tensor_name  # Unused by updated loading code.
+    Additionally, the graph is pruned of all nodes that are not needed for the specified outputs.
 
-  if not gfile.Exists(input_graph):
-    raise ValueError("Input graph file '" + input_graph + "' does not exist!")
+    -------------------------------------------------------
+    NOTE: this function creates new nodes in the default graph, you may want to wrap the call with the following code
+    -------------------------------------------------------
+    with tf.Graph().as_default():
+        freeze_graph(...)
+    -------------------------------------------------------
 
-  if input_saver and not gfile.Exists(input_saver):
-    raise ValueError("Input saver file '" + input_saver + "' does not exist!")
+    :param input_graph: path to the input graph file
+    :param input_checkpoint: path to the input checkpoint
+    :param output_node_names: iterable collection of output node names
+    :param output_graph: path to the output frozen graph file
 
-  # 'input_checkpoint' may be a prefix if we're using Saver V2 format
-  if not saver_lib.checkpoint_exists(input_checkpoint):
-    raise ValueError("Input checkpoint '" + input_checkpoint + "' doesn't exist!")
+    Raises:
+        ValueError: if any of the specified files does not exist
+    """
 
-  if not output_node_names:
-    raise ValueError("You need to supply the name of a node to --output_node_names.")
+    if not gfile.Exists(input_graph):
+        raise ValueError('Input graph file `{}` does not exist!'.format(input_graph))
 
-  input_graph_def = graph_pb2.GraphDef()
-  mode = "rb" if input_binary else "r"
-  with gfile.FastGFile(input_graph, mode) as f:
-    if input_binary:
-      input_graph_def.ParseFromString(f.read())
-    else:
-      text_format.Merge(f.read(), input_graph_def)
-  # Remove all the explicit device specifications for this node. This helps to
-  # make the graph more portable.
-  if clear_devices:
+    # 'input_checkpoint' may be a prefix if we're using Saver V2 format
+    if not saver_lib.checkpoint_exists(input_checkpoint):
+        raise ValueError('Input checkpoint `{}` does not exist!'.format(input_checkpoint))
+
+    # read the graph definition
+    input_graph_def = graph_pb2.GraphDef()
+    with gfile.FastGFile(input_graph, 'rb') as file:
+        input_graph_def.ParseFromString(file.read())
+
+    # remove all the explicit device specifications for this node. This helps to make the graph more portable.
     for node in input_graph_def.node:
-      node.device = ""
+        node.device = ''
 
-  _ = importer.import_graph_def(input_graph_def, name="")
-
-  with session.Session() as sess:
-    if input_saver:
-      with gfile.FastGFile(input_saver, mode) as f:
-        saver_def = saver_pb2.SaverDef()
-        if input_binary:
-          saver_def.ParseFromString(f.read())
-        else:
-          text_format.Merge(f.read(), saver_def)
-        saver = saver_lib.Saver(saver_def=saver_def)
+    # restore the input graph and checkpoint
+    _ = importer.import_graph_def(input_graph_def, name='')
+    with session.Session() as sess:
+        var_list = {}
+        reader = pywrap_tensorflow.NewCheckpointReader(input_checkpoint)
+        var_to_shape_map = reader.get_variable_to_shape_map()
+        for key in var_to_shape_map:
+            try:
+                tensor = sess.graph.get_tensor_by_name(key + ':0')
+            except KeyError:
+                # This tensor doesn't exist in the graph (for example it's
+                # 'global_step' or a similar housekeeping element) so skip it.
+                continue
+            var_list[key] = tensor
+        saver = saver_lib.Saver(var_list=var_list)
         saver.restore(sess, input_checkpoint)
-    else:
-      var_list = {}
-      reader = pywrap_tensorflow.NewCheckpointReader(input_checkpoint)
-      var_to_shape_map = reader.get_variable_to_shape_map()
-      for key in var_to_shape_map:
-        try:
-          tensor = sess.graph.get_tensor_by_name(key + ":0")
-        except KeyError:
-          # This tensor doesn't exist in the graph (for example it's
-          # 'global_step' or a similar housekeeping element) so skip it.
-          continue
-        var_list[key] = tensor
-      saver = saver_lib.Saver(var_list=var_list)
-      saver.restore(sess, input_checkpoint)
-      if initializer_nodes:
-        sess.run(initializer_nodes)
 
-    variable_names_blacklist = (variable_names_blacklist.split(",") if
-                                variable_names_blacklist else None)
-    output_graph_def = graph_util.convert_variables_to_constants(
-        sess,
-        input_graph_def,
-        output_node_names.split(","),
-        variable_names_blacklist=variable_names_blacklist)
+        # convert all the variables to constants
+        with DisabledLogger('tensorflow'), DisabledPrint():
+            output_graph_def = graph_util.convert_variables_to_constants(sess, input_graph_def, output_node_names)
 
-  with gfile.GFile(output_graph, "wb") as f:
-    f.write(output_graph_def.SerializeToString())
-  logging.debug('%d ops in the final graph." % len(output_graph_def.node)')
+    with gfile.GFile(output_graph, 'wb') as file:
+        file.write(output_graph_def.SerializeToString())
