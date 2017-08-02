f6cd0a65f68f39f479315a4373413cbb704ea695
Jan Nováček <gnovis@users.noreply.github.com>
2017-08-02 11:11:16 +0200

    TrainCheckHookTest done. (#43)
    TrainCheckHookTest done.

---
+++ cxflow/tests/hooks/train_check_hook_test.py
@@ -0,0 +1,76 @@
+"""
+Test module for train check hook (cxflow.hooks.train_check_hook).
+"""
+
+import numpy as np
+import collections
+
+from cxflow.tests.test_core import CXTestCase
+from cxflow.hooks.train_check_hook import TrainCheckHook
+from cxflow.hooks.abstract_hook import TrainingTerminated
+
+
+_VAR = "accuracy"
+_MIN_ACCURACY = 0.95
+_MAX_EPOCH = 10
+_CURRENT_EPOCH = 5
+
+
+def _get_epoch_data():
+    epoch_data = collections.OrderedDict([
+        ('train', collections.OrderedDict([
+            ('accuracy', 1),
+        ])),
+        ('test', collections.OrderedDict([
+            ('accuracy', 0.5),
+        ])),
+        ('valid', collections.OrderedDict([
+            ('accuracy', np.ones(10)),
+        ]))
+    ])
+    return epoch_data
+
+
+class TrainCheckHookTest(CXTestCase):
+    """Test case for TrainCheckHook."""
+
+    def setUp(self):
+        """Create new epoch data for each test separately."""
+        self._epoch_data = _get_epoch_data()
+
+
+    def test_stream_raise(self):
+        """Test raising error, when stream not in epoch_data."""
+        hook = TrainCheckHook(_VAR, _MIN_ACCURACY, _MAX_EPOCH, "unknown")
+        with self.assertRaises(KeyError):
+            hook.after_epoch(_CURRENT_EPOCH, self._epoch_data)
+
+    def test_variable_raise(self):
+        """Test raising error, when variable not in stream."""
+        hook = TrainCheckHook("not_present", _MIN_ACCURACY, _MAX_EPOCH)
+        with self.assertRaises(KeyError):
+            hook.after_epoch(_CURRENT_EPOCH, self._epoch_data)
+
+    def test_not_scalar_raise(self):
+        """Test raising error, when variable is not scalar."""
+        hook = TrainCheckHook(_VAR, _MIN_ACCURACY, _MAX_EPOCH)
+        with self.assertRaises(ValueError):
+            hook.after_epoch(_CURRENT_EPOCH, self._epoch_data)
+
+    def test_training_terminated(self):
+        """
+        Test whether training terminates if the given
+        stream variable exceeds the threshold.
+        """
+        hook = TrainCheckHook(_VAR, _MIN_ACCURACY, _MAX_EPOCH, "train")
+        with self.assertRaises(TrainingTerminated):
+            hook.after_epoch(_CURRENT_EPOCH, self._epoch_data)
+
+    def test_epoch_raise(self):
+        """
+        Test raising error, when required value of
+        stream variable wasn't reached in specified count of epochs.
+        """
+        hook = TrainCheckHook(_VAR, _MIN_ACCURACY, _MAX_EPOCH, "test")
+        with self.assertRaises(ValueError):
+            hook.after_epoch(11, self._epoch_data)
