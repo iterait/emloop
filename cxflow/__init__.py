1af3bd70a896d0f3107a6304975d80b6c7e60dc0
blazeka <blazekada@gmail.com>
2017-08-28 14:09:34 +0200

    Add __version__ to __init__.py (closes #82)

--- cxflow/__init__.py
+++ cxflow/__init__.py
@@ -3,3 +3,5 @@ from .entry_point import train
 from .datasets import AbstractDataset, BaseDataset
 from .main_loop import MainLoop
 from .models.abstract_model import AbstractModel
+
+__version__ = '0.7.0'
