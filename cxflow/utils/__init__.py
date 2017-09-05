4390bc4264da3f3ec3b13d3783ec83b87bc5c4ec
Petr Bělohlávek <petrbel@users.noreply.github.com>
2017-09-05 15:43:36 +0200

    Downloadable dataset (#110)
    * Downloadable dataset
    
    * Progress bar
    
    * Add requests to reqs.txt
    
    * Pass arguments to downloadable dataset `_configure_dataset`
    
    * Docstring change

--- cxflow/utils/__init__.py
+++ cxflow/utils/__init__.py
@@ -1,4 +1,5 @@
 from .config import parse_arg, load_config, config_to_file, config_to_str
+from .download import maybe_download_and_extract
 from .misc import DisabledLogger, DisabledPrint
 from .profile import Timer
 from .reflection import _EMPTY_DICT, parse_fully_qualified_name, create_object, list_submodules, find_class_module,\
---
+++ cxflow/utils/download.py
@@ -0,0 +1,48 @@
+import click
+import logging
+import os
+import urllib.request
+import re
+import requests
+import tarfile
+from typing import Iterable
+import shutil
+import zipfile
+
+
+def maybe_download_and_extract(data_root: str, url_root: str, filenames: Iterable[str]) -> None:
+    """
+    Maybe download the specified files from ``url_root`` to ``download_root`` and
+        - unzip any ``.zip`` archives
+        - extract any ``.tar.*`` archives based on :py:func:`tarfile.open`
+    :param data_root: data root to download the files to
+    :param url_root: root url
+    :param filenames: list of filenames to download
+    """
+
+    os.makedirs(data_root, exist_ok=True)
+
+    for filename in filenames:
+        # check whether the archive already exists
+        filepath = os.path.join(data_root, filename)
+        if os.path.exists(filepath):
+            logging.info('\t`%s` already exists; skipping', filepath)
+            continue
+
+        # download with progressbar
+        logging.info('\tdownloading %s', filepath)
+        if not url_root.endswith('/'):
+            url_root += '/'
+
+        req = requests.get(url_root + filename, stream=True)
+        expected_size = int(req.headers.get('content-length'))
+        chunk_size = 1024
+        with open(filepath, 'wb') as f_out,\
+             click.progressbar(req.iter_content(chunk_size=chunk_size), length=expected_size/chunk_size) as bar:
+            for chunk in bar:
+                if chunk:
+                    f_out.write(chunk)
+                    f_out.flush()
+
+        # extract
+        shutil.unpack_archive(filepath, data_root)
