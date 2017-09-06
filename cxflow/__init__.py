f61bce5ffdfa65f61c1a8df30af0ee236dfd15fe
Adam Blažek <adam.blazek@cognexa.com>
2017-09-06 16:17:31 +0200

    Add AbstractHook, EpochData, Stream and Batch to cxflow API (#114)

--- cxflow/__init__.py
+++ cxflow/__init__.py
@@ -1,9 +1,14 @@
 """**cxflow** core module."""
 from .entry_point import train
 from .datasets import AbstractDataset, BaseDataset
+from .hooks import AbstractHook
 from .main_loop import MainLoop
 from .models.abstract_model import AbstractModel
 from . import constants
+
+Batch = AbstractDataset.Batch
+Stream = AbstractDataset.Stream
+EpochData = AbstractHook.EpochData
 
 __all__ = ['MainLoop']
 
