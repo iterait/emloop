15454649c3c79c0005737c5272ecd7760c1ea207
Jan Nováček <novacekj5@gmail.com>
2017-10-01 11:43:53 +0200

    added test case for EveryNEpoch hook

---
+++ cxflow/tests/hooks/every_n_epoch_test.py
@@ -0,0 +1,33 @@
+"""
+Test module for EveryNEpoch hook (cxflow.hooks.every_n_epoch).
+"""
+
+from cxflow.tests.test_core import CXTestCase
+from cxflow.hooks.every_n_epoch import EveryNEpoch
+
+
+class DummyHook(EveryNEpoch):
+    """Dummy hook which inherits from :py:class:`cxflow.hooks.EveryNEpoch` hook."""
+
+    def __init__(self, **kwargs):
+        """Create new DummyHook."""
+        super().__init__(**kwargs)
+        self._epoch_ids = []
+
+    def _after_n_epoch(self, epoch_id):
+        """After every n epoch append ``epoch_id`` to the ``_epoch_ids`` list."""
+        self._epoch_ids.append(epoch_id)
+
+
+class EveryNEpochTest(CXTestCase):
+    """Test case for :py:class:`cxflow.hooks.EveryNEpoch` hook."""
+
+    def test_after_n_epoch(self):
+        """Test whether ``_after_n_epoch`` method is called every third epoch."""
+
+        hook = DummyHook(n_epochs=3)
+
+        for i in range(9):
+            hook.after_epoch(i)
+
+        self.assertEqual(hook._epoch_ids, [0, 3, 6])
